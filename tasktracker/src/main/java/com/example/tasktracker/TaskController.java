e6be1e6be804c61831bf3a2bf87798606a3615a7
Sharath <vickysharath4@gmail.com>
2026-04-13 12:21:06 -0400

    Initial Spring Boot task tracker setup

--- tasktracker/src/main/java/com/example/tasktracker/TaskController.java
+++ tasktracker/src/main/java/com/example/tasktracker/TaskController.java
@@ -1,22 +1,32 @@
 package com.example.tasktracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class TaskController {
-    @GetMapping("/Task")
-    public  List<Task> getTasks(){
-        return List.of(
-                new Task(
-                        101L,
-                        "Fuck off",
-                        "Create First Version",
-                        "IN_Progress",
-                        "Vicky"
-                )
-        );
+    private final List<Task> tasks = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public TaskController() {
+        tasks.add(new Task(101L, "SetupLogin page", "Created First Varsiom", "In_Progress","Bivky"));
+        nextId = 104L;
     }
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+        return tasks;
+    }
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
+        task.setId(nextId);
+        nextId++;
+        tasks.add(task);
+        return task;
+    }
+
 }
